409e02718b152cfc402b335ad73a5ed6fe30d378
TijnvanHeertum <tijnvanheertum@hotmail.com>
2024-02-18 10:43:41 +0100

    Added a plot_camera_position function, annotated functions, gridsize to 9x6

--- calibration_modular.py
+++ calibration_modular.py
@@ -3,7 +3,14 @@ import numpy as np
 import cv2 as cv
 import glob
 import os
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
+# This function iterates over all images in the training folder and attempts to find the chessboard corners manually.
+# If it can't find them it opens a window to add them manually. It then saves the object and image points.
+# Then it performs camera calibration if the remove_bad_images is set to True. It then calculates the re-projection error after the addition of the training image
+# and compares it to the re-projection error before adding this image. If the error increases, the object and image points are removed again and the image is not used.
+# This function returns the object points and image points of the 'good' images (if remove_bad_features = False, it saves them from all images). 
 def find_chessboard_corners(image_folder, remove_bad_images=False):
     # Arrays to store object points and image points from all the images.
     objpoints = [] # 3d point in real world space
@@ -16,14 +23,14 @@ def find_chessboard_corners(image_folder, remove_bad_images=False):
     square_size = 16.6
 
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((6*7,3), np.float32)
-    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2) * square_size
+    objp = np.zeros((6*9,3), np.float32)
+    objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2) * square_size
 
     # Get the list of image files
     images = glob.glob(os.path.join(image_folder, '*.jpeg'))
 
     # Initial calibration 
-    ret, mtx, dist, rvecs, tvecs = None, None, None, None, None
+    ret, mtx, dst, rvecs, tvecs = None, None, None, None, None
     prev_error = float('inf')
 
     #iterate over images to find chessboard corners and add image and object points automatically if possible, otherwise manually.
@@ -32,7 +39,7 @@ def find_chessboard_corners(image_folder, remove_bad_images=False):
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
         # Find the chess board corners
-        ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+        ret, corners = cv.findChessboardCorners(gray, (9,6), None)
 
         # If found, add object points, image points (after refining them)
         if ret == True:
@@ -41,17 +48,17 @@ def find_chessboard_corners(image_folder, remove_bad_images=False):
             imgpoints.append(corners2)
 
             # Draw and display the corners
-            cv.drawChessboardCorners(img, (7,6), corners2, ret)
+            cv.drawChessboardCorners(img, (9,6), corners2, ret)
             cv.imshow('img', img)
             cv.waitKey(500)
 
             # If bad images need to be excluded, calculate reprojection error after addition of each image, if it increases, dont use the image.
             if remove_bad_images:
                 # Calibrate camera with the new image
-                ret_new, mtx_new, dist_new, rvecs_new, tvecs_new = calibrate_camera(objpoints, imgpoints, gray.shape[::-1])
+                ret_new, mtx_new, dst_new, rvecs_new, tvecs_new = calibrate_camera(objpoints, imgpoints, gray.shape[::-1])
 
                 # Calculate reprojection error
-                error = calculate_reprojection_error(objpoints, imgpoints, rvecs_new, tvecs_new, mtx_new, dist_new)
+                error = calculate_reprojection_error(objpoints, imgpoints, rvecs_new, tvecs_new, mtx_new, dst_new)
 
                 # Check if the error increased
                 if ret_new and error >= prev_error:
@@ -60,10 +67,10 @@ def find_chessboard_corners(image_folder, remove_bad_images=False):
                     imgpoints.pop()
                 else:
                     # Update calibration results
-                    ret, mtx, dist, rvecs, tvecs = ret_new, mtx_new, dist_new, rvecs_new, tvecs_new
+                    ret, mtx, dst, rvecs, tvecs = ret_new, mtx_new, dst_new, rvecs_new, tvecs_new
                     prev_error = error
             else:
-                ret, mtx, dist, rvecs, tvecs = calibrate_camera(objpoints, imgpoints, gray.shape[::-1])
+                ret, mtx, dst, rvecs, tvecs = calibrate_camera(objpoints, imgpoints, gray.shape[::-1])
 
         else:
             #if not found, set cornerpoints to mouseclicks, adding them as image points and object points.
@@ -73,10 +80,10 @@ def find_chessboard_corners(image_folder, remove_bad_images=False):
 
             if remove_bad_images:
                 # Calibrate camera with the new image
-                ret_new, mtx_new, dist_new, rvecs_new, tvecs_new = calibrate_camera(objpoints, imgpoints, gray.shape[::-1])
+                ret_new, mtx_new, dst_new, rvecs_new, tvecs_new = calibrate_camera(objpoints, imgpoints, gray.shape[::-1])
 
                 # Calculate reprojection error
-                error = calculate_reprojection_error(objpoints, imgpoints, rvecs_new, tvecs_new, mtx_new, dist_new)
+                error = calculate_reprojection_error(objpoints, imgpoints, rvecs_new, tvecs_new, mtx_new, dst_new)
 
                 # Check if the error increased
                 if ret_new and error >= prev_error:
@@ -85,13 +92,14 @@ def find_chessboard_corners(image_folder, remove_bad_images=False):
                     imgpoints.pop()
                 else:
                     # Update calibration results
-                    ret, mtx, dist, rvecs, tvecs = ret_new, mtx_new, dist_new, rvecs_new, tvecs_new
+                    ret, mtx, dst, rvecs, tvecs = ret_new, mtx_new, dst_new, rvecs_new, tvecs_new
                     prev_error = error
             else:
-                ret, mtx, dist, rvecs, tvecs = calibrate_camera(objpoints, imgpoints, gray.shape[::-1])
+                ret, mtx, dst, rvecs, tvecs = calibrate_camera(objpoints, imgpoints, gray.shape[::-1])
     cv.destroyAllWindows()
     return objpoints, imgpoints
 
+# Helper function to manually select cornerpoints. It shows the coordinates of the clicked points and saves them as image points.
 def click_event(event, x, y, flags, param, img, objp, imgpoints, objpoints):
     if event == cv.EVENT_LBUTTONDOWN:
 
@@ -104,11 +112,13 @@ def click_event(event, x, y, flags, param, img, objp, imgpoints, objpoints):
         imgpoints.append((x, y))
         objpoints.append(objp)
 
+# This function calibrates the camera based on the object points and image points and returns the calibration settins (camera matrix, distortion matrix, etc.)
 def calibrate_camera(objpoints, imgpoints, shape):
     # getting the camera matrix, distortion coefficients, rotation and translation vectors
-    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, shape, None, None)
-    return ret, mtx, dist, rvecs, tvecs
+    ret, mtx, dst, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, shape, None, None)
+    return ret, mtx, dst, rvecs, tvecs
 
+# This function undistorts the images using the calibration setting and saves the undistorted images in a new folder.
 def undistort_images(image_folder, undistorted_folder, mtx, dst):
     images = glob.glob(os.path.join(image_folder, '*.jpeg'))
     for fname in images:
@@ -129,13 +139,32 @@ def undistort_images(image_folder, undistorted_folder, mtx, dst):
         output_path = os.path.join(undistorted_folder, 'undistorted_' + os.path.basename(fname))
         cv.imwrite(output_path, dst)
 
-def calculate_reprojection_error(objpoints, imgpoints, rvecs, tvecs, mtx, dist):
+# This function calculates the mean re-projection error by re-projecting the image points using the object points, rvecs, tvecs, mtx, and dst and comparing it to the earlier image points.
+def calculate_reprojection_error(objpoints, imgpoints, rvecs, tvecs, mtx, dst):
     mean_error = 0
     for i in range(len(objpoints)):
-        imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dst)
         error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2) / len(imgpoints2)
         mean_error += error
     return mean_error / len(objpoints)
+
+# This function plots the camera positions of all training images by plotting the translation vectors as points and then adding a quiver based on the rotation vector.
+def plot_camera_positions(image_folder, rvecs, tvecs):
+    # create 3d figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # For each tvec, add a scatter based on x[0], y[1], z[2] coordinates.
+    for i, (tvec, rvec) in enumerate(zip(tvecs, rvecs)):
+        ax.scatter(tvec[0], tvec[1], tvec[2], c='r', marker='o')  # camera position
+        ax.text(tvec[0], tvec[1], tvec[2], image_folder[i]) #train image annotation
+        ax.quiver(tvec[0], tvec[1], tvec[2], rvec[0], rvec[1], rvec[2], length=0.1, color='b')  # rotation vector
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title('Camera position of training images')
+    plt.show()
 
 def main(remove_bad_images = False):
     # Get the current working directory
@@ -149,19 +178,22 @@ def main(remove_bad_images = False):
     os.makedirs(undistorted_folder, exist_ok=True)
 
     #get obj and img points
-    objpoints, imgpoints, gray, ret, mtx, dist, rvecs, tvecs = find_chessboard_corners(image_folder, remove_bad_images)
+    objpoints, imgpoints, gray, ret, mtx, dst, rvecs, tvecs = find_chessboard_corners(image_folder, remove_bad_images)
 
     #calibrate camera and save camera matrix and dst coefficients
-    ret, mtx, dist, rvecs, tvecs = calibrate_camera(objpoints, imgpoints, gray.shape[::-1])
+    ret, mtx, dst, rvecs, tvecs = calibrate_camera(objpoints, imgpoints, gray.shape[::-1])
     np.savetxt('camera_matrix.txt', mtx)
-    np.savetxt('distortion_coefficients.txt', dist)
+    np.savetxt('distortion_coefficients.txt', dst)
 
     #undistort images
-    undistort_images(image_folder, undistorted_folder, mtx, dist)
+    undistort_images(image_folder, undistorted_folder, mtx, dst)
 
     #calculate re-projection error
-    mean_error = calculate_reprojection_error(objpoints, imgpoints, rvecs, tvecs, mtx, dist)
+    mean_error = calculate_reprojection_error(objpoints, imgpoints, rvecs, tvecs, mtx, dst)
     print("total error:", mean_error)
+
+    # plot camera positions
+    plot_camera_positions(image_folder, rvecs, tvecs)
 
     
 if __name__ == "__main__":
